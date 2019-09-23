6da4f20551cb5da1732be97fe10c04522ac3e79a
Janell Huyck <janell.huyck@outlook.com>
2019-09-23 08:54:29 -0400

    initial commit. some test ideas in comments

---
+++ test.js
@@ -0,0 +1,11 @@
+/* tests to run:
+
+location of player is outside board boundaries
+location of player is within a wall
+location of player is at finish and no message comes up
+player doesn't move on arrow keypress
+player isn't moved to start on press of "reset"
+cannot determine player location
+no maze found
+
+*/
