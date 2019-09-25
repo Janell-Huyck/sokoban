82c3812700383be1f7ba4583d09c7ad280c0832e
Janell Huyck <janell.huyck@outlook.com>
2019-09-25 11:34:36 -0400

    updated comments on 'test'

--- test.js
+++ test.js
@@ -1,17 +1,6 @@
-/* tests to run:
-
-location of player is outside board boundaries
-location of player is within a wall
-location of player is at finish and no message comes up
-player doesn't move on arrow keypress
-player isn't moved to start on press of "reset"
-cannot determine player location
-no maze found
-
-*/
+//Sadly, I could not figure out how to get tests to work for this program beyond this one.
+//Since this was my first attempt at TDD, I'm going to try again with the next project!
 
 //cannot determine player location - location is underined
 console.assert(bearLocation !== undefined, "We have lost the bear!  His location is 'undefined'!");
 
-// console.assert(bearLocation === [], "The bear needs a home!  His location is []...");  
-//that test only looks at location at the start of the program, before the maze is even run!  Not helpful!
