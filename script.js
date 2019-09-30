b946354ee18f0ed1a1f28b09764e3c675daae716
Janell Huyck <janell.huyck@outlook.com>
2019-09-29 23:29:32 -0400

    beautiful, working project!

--- script.js
+++ script.js
@@ -1,283 +1,277 @@
-// let bearLocation = []; // where on the map the player's bear is.  expressed as [row, column]
-// let oldBearLocation = []; //used when moving bear
-// let startLocation = []; // [row, column])
-// let finishLocation = []; // [row, column])
-
-let treeImage = "img/tree.png";
-let bearImage = "img/bear.png";
-let blankImage = "img/blank.jpg";
-let fulljarImage = "img/fulljar.png"
-let emptyjarImage = "img/emptyjar.png"
-let honeyImage = "img/honey.png"
-
-
-// in the maze, T = a tree (inpassable), B = bear - starting location , H = honey location, and J = jar location
+// in the forest, T = a tree (inpassable), B = bear - starting location , H = honey location, and J = jar location
 // C = background to make clear
 let forest = [
-   "CCCCTTTTTCCCCCCCCCC",  
-   "CCCCT   TCCCCCCCCCC",  
-   "CCCCTJ  TCCCCCCCCCC",  
-   "CCTTT  JTTCCCCCCCCC",  
-   "CCT  J J TCCCCCCCCC",  
-   "TTT T TT TCCCTTTTTT",  
-   "T   T TT TTTTT  HHT",  
-   "T J  J          HHT",  
-   "TTTTT TTT TBTT  HHT",  
-   "CCCCT     TTTTTTTTT",  
-   "CCCCTTTTTTTCCCCCCCC"  
+  "CCCCTTTTTCCCCCCCCCC",
+  "CCCCT   TCCCCCCCCCC",
+  "CCCCTJ  TCCCCCCCCCC",
+  "CCTTT  JTTCCCCCCCCC",
+  "CCT  J J TCCCCCCCCC",
+  "TTT T TT TCCCTTTTTT",
+  "T   T TT TTTTT  HHT",
+  "T J  J          HHT",
+  "TTTTT TTT TBTT  HHT",
+  "CCCCT     TTTTTTTTT",
+  "CCCCTTTTTTTCCCCCCCC"
 ];
 
+let bearLocationArray; //a bear-tracker!
+let jarCounter = 1;  // used simply to make an id for jars so that they can interact with honey squares properly
+
+
+//=============================================================================================
+// defining and prototyping ForestSquareImage
+//==========================================================================================
+
+function ForestSquareImage(row, column, forestLetter, imageUrl) {
+  this.img = document.createElement("img");
+  // this.img.location = [row , column];
+  this.img.forestLetter = forestLetter;
+  if (this.img.forestLetter === "H") {
+    this.img.forestletter = " "; //treat honey as open space.
+  }
+  if (this.img.forestLetter === "B") {
+    bearLocationArray = [row, column]; //initial setting of bearLocationArray - only done once.
+    this.img.id = "bear";
+  }
+
+  if (this.img.forestLetter === "J") {
+    this.img.id = "jar" + jarCounter; //in CSS, i need to set jar opacity to 1 to override the honey settings
+    jarCounter++;
+  }
+  this.img.src = imageUrl;
+  this.img.className = "forestSquareImage " + imageUrl.slice(4, -4);
+  targetDiv = document.getElementById("div" + row + "_" + column);
+  targetDiv.appendChild(this.img);
+}
+
+ForestSquareImage.prototype = {
+  constructor: ForestSquareImage,
+  moveSelf
+};
+
+//=====================================================================================================
+//These functions are properties of the object ForestSquareImage
+//=================================================================================================
+
+
+
+function moveSelf(oldLocation, newLocation) {
+  //input format is ([oldRow, oldColumn] , [newRow, newColumn])
+
+  newDivLocation = document.getElementById(
+    "div" + newLocation[0] + "_" + newLocation[1]
+  );
+  oldDivLocation = document.getElementById(
+    "div" + oldLocation[0] + "_" + oldLocation[1]
+  );
+
+  pictureThatIsMoving = oldDivLocation.removeChild(oldDivLocation.firstChild);
+  whatWeTookOutOfNewLocation = newDivLocation.removeChild(
+    newDivLocation.firstChild
+  );
+
+  newDivLocation.appendChild(pictureThatIsMoving);
+  oldDivLocation.appendChild(whatWeTookOutOfNewLocation);
+}
+
+//=======================================================================
+// Create The Initial Forest
+//======================================================================
 
 function drawForest() {
-  document.getElementById("thisIsTheForest").innerHTML=""
+  document.getElementById("thisIsTheForest").innerHTML = "";
   for (let forestRow = 0; forestRow < forest.length; forestRow++) {
-    drawForestRow(forestRow)
+    drawForestRow(forestRow);
   }
 }
 
 //append a div onto the thisIsTheForest div that will hold a row of forest.
 function drawForestRow(row) {
-  forestRowDiv = document.createElement('div');
+  forestRowDiv = document.createElement("div");
   forestRowDiv.className = "row";
   forestRowDiv.id = "row" + row;
   destination = document.getElementById("thisIsTheForest");
   destination.appendChild(forestRowDiv);
-  drawForestColumns(row)
+  drawForestColumns(row);
 }
 
+function makeForestSquareDiv(initialLetter, row, column) {
+  forestSquareDiv = document.createElement("div");
+  forestSquareDiv.initialLetter = initialLetter;
+  forestSquareDiv.className = "forestSquareDiv " + initialLetter;
+  forestSquareDiv.id = "div" + row + "_" + column;
+  forestSquareDiv.location = [row, column];
+  destination = document.getElementById("row" + row);
+  destination.appendChild(forestSquareDiv);
+}
 
 function drawForestColumns(row) {
-  for (let forestColumn = 0; forestColumn < forest[row].length; forestColumn++){
-    squareDiv = document.createElement('div');
-    squareDiv.id = "row" + row + "square" + forestColumn;
-    squareDiv.style.backgroundImage = determineImageToDraw(row, forestColumn);
-    squareDiv.className = "square " + squareDiv.style.backgroundImage.slice(9, -6);
-    squareDiv.row = row;
-    squareDiv.column = forestColumn;
+  for (
+    let forestColumn = 0;
+    forestColumn < forest[row].length;
+    forestColumn++
+  ) {
+    initialLetter = findInitialLetter(row, forestColumn);
+    //creates the outside div of the individual column div
+    makeForestSquareDiv(initialLetter, row, forestColumn);
 
-    // squareDiv.topNeighbor = determineTopNeighbor(row, forestColumn);
-    // squareDiv.rightNeighbor = determineRightNeighbor(row, forestColumn);
-    // squareDiv.leftNeighbor = determineLeftNeighbor(row, forestColumn);
-    // squareDiv.bottomNeighbor = determineBottomNeighbor(row, forestColumn);
-    squareDiv.location = [row , forestColumn];
-    // squareDiv.weight = determineSquareWeight(row, forestColumn);
-    // squareDiv.hasJar = determineJarStatus(row, forestColumn);
-    // squareDiv.hasTree = determineTreeStatus(row, forestColumn);
-    // squareDiv.hasBear = determineBearStatus(row, forestColumn);
-    // squareDiv.hasHoney = determineHoneyStatus(row, forestColumn);
-    destination = document.getElementById("row" + row);
-    destination.appendChild(squareDiv)
-    // drawSquare(squareDiv);
+    //creates the inside of the individual column div
+    imageUrl = determineImageToDraw(initialLetter);
+    const forestSquareImage = new ForestSquareImage(
+      row,
+      forestColumn,
+      initialLetter,
+      imageUrl
+    );
   }
 }
 
-function drawSquare(currentSquare){
-  currentSquare.style.backgroundImage="url(currentSquare.image)";
+function findInitialLetter(row, column) {
+  forestRowString = forest[row].slice(); //return shallow copies of the row and item
+  forestLetter = forestRowString[column].slice();
+  return forestLetter;
 }
 
-function fetchLetterFromGrid(row,column){
-  gridLetter = forest[row][column]
-  return gridLetter
-}
+//=========================================================================
+// Dealing with the Jars
+//=========================================================================
 
-function determineImageToDraw(row,column){
-  gridLetter = fetchLetterFromGrid(row,column)
-  if (gridLetter === " "){
-    return "url('img/blank.png')"
-  } else if (gridLetter === "T") {
-    return "url('img/tree.png')"
-  } else if (gridLetter === "B") {
-    return "url('img/bear.png')"
-  } else if (gridLetter === "H") {
-    return "url('img/honey.png')"
-  } else if (gridLetter === "J"){
-    return "url('img/emptyjar.png')"
-  } else if (gridLetter === "C") {
-    return ""
+
+function toggleJarPicture(locationOfJar) {
+  jarLocationDiv = getTheDivForThisLocation(locationOfJar)
+  if (jarLocationDiv.initialLetter === "H") {
+    jarLocationDiv.firstChild.src = "img/fulljar.png"
+    jarLocationDiv.firstChild.className = "forestSquareImage fullJar"
+    
+  } else {
+    jarLocationDiv.firstChild.src = "img/emptyjar.png"
+    jarLocationDiv.firstChild.className = "forestSquareImage emptyJar"
   }
 }
 
-function determineTopNeighbor(row,column){
-
+function checkForWin(){
+  allTheFullJars = document.querySelectorAll(".fullJar")
+  if (allTheFullJars.length === 6){
+    showPlayerHasWon()
+  }
 }
 
-function determineRightNeighbor(row,column){
-
+function showPlayerHasWon(){
+  document.getElementById("youWon").style.display = "inline-block"
 }
 
-function determineLeftNeighbor(row,column) {
-
+function moveJar(jarLocation, direction) {
+  jarNeighborArray = returnNextCoordinates (jarLocation, direction)
+  jarNeighborContents = returnWhatIsInThisLocation(jarNeighborArray)
+  if (jarNeighborContents === " " || jarNeighborContents === "H") {
+    moveSelf(jarLocation, jarNeighborArray)  //move the jar and then...
+    toggleJarPicture(jarNeighborArray) // switch to full or empty jar and then...
+    moveSelf(bearLocationArray, whereBearWantsToMove) //move the bear where the jar was
+    bearLocationArray = whereBearWantsToMove.slice()  //update bearLocationArray after moving bear
+    checkForWin()
+  } else return
 }
 
-function determineBottomNeighbor(row,column) {
-
+//===============================================================
+// Utility functions used several places
+//==============================================================
+function determineImageToDraw(letter) {
+  if (letter === " ") {
+    return "img/forestFloor.png";
+  } else if (letter === "T") {
+    return "img/tree.png";
+  } else if (letter === "B") {
+    return "img/bear.png";
+  } else if (letter === "H") {
+    return "img/forestFloor.png"; //honey will be drawn with background image on the html div
+  } else if (letter === "J") {
+    return "img/emptyjar.png";
+  } else if (letter === "C") {
+    return "img/blank.png";
+  }
 }
 
-function determineSquareWeight(row,column) {
+function returnNextCoordinates(startingArray, direction) {
+  if (direction === "up") {
+    return [parseInt(startingArray.slice(0,1),10)-1, parseInt(startingArray.slice(1),10) ]
+  } else if (direction === "down") {
+    return [parseInt(startingArray.slice(0,1),10)+1, parseInt(startingArray.slice(1),10) ]
+  } else if (direction === "left") {
+    return [parseInt(startingArray.slice(0,1),10), parseInt(startingArray.slice(1),10) -1 ]
+  } else if (direction === "right") {
+    return [parseInt(startingArray.slice(0,1),10), parseInt(startingArray.slice(1),10) +1 ]
+  } else console.log ("error in returnNextCoordinates function")
+}
 
+function returnWhatIsInThisLocation(location) {
+  thisLocationDiv = getTheDivForThisLocation(location)
+  whatIsInThisLocation = thisLocationDiv.firstChild.forestLetter
+  return whatIsInThisLocation
+}
+
+function getTheDivForThisLocation(location){
+  theDiv = document.getElementById("div" + location[0] + "_" + location[1] )
+  return theDiv
+}
+
+//====================================================================================
+// Moving the Bear
+//====================================================================================
+
+function moveBear(e) {
+  keyPressed = determineWhatKeyIsPressed(e);
+  if (keyPressed === "") { return } //guard against anything but up/down/right/left
+  direction = keyPressed  
+  whereBearWantsToMove = returnNextCoordinates (bearLocationArray , direction)
+  whatIsWhereBearWantsToMove = returnWhatIsInThisLocation(whereBearWantsToMove)
+  if (whatIsWhereBearWantsToMove === "J") {
+    moveJar(whereBearWantsToMove, direction)
+  } else if (whatIsWhereBearWantsToMove === " " || whatIsWhereBearWantsToMove === "H") {
+    moveSelf(bearLocationArray, whereBearWantsToMove)
+    bearLocationArray = whereBearWantsToMove.slice()
+  }
+}
+
+//============================================================================
+// Things that interact with the keyboard/mouse
+//===========================================================================
+
+function determineWhatKeyIsPressed(e) {
+  console.log(e.key);
+  return e.key === "ArrowUp" || e.key === "w" || e.key === "W"
+  ? "up"
+  : e.key === "ArrowDown" || e.key === "s" || e.key === "S"
+  ? "down"
+  : e.key === "ArrowRight" || e.key === "d" || e.key === "D"
+  ? "right"
+  : e.key === "ArrowLeft" || e.key === "a" || e.key === "A"
+  ? "left"
+  : "";
+}
+
+function startOver() {
+  window.location.reload();
 }
 
 
+//reloads the page when "startOver" button is clicked
+document.getElementById("startOver").addEventListener("click", startOver);
 
+//show instructions when "instructionsButton" is clicked
+document.getElementById("instructionsButton").addEventListener("click", e => {
+  document.getElementById("instructionsToHide").style.display = "inline-block";
+  document.getElementById("instructionsButton").style.display = "none";
+});
+
+//hide instructions when "hideInstructions" button is clicked
+document.getElementById("hideInstructions").addEventListener("click", () => {
+  document.getElementById("instructionsToHide").style.display = "none";
+  document.getElementById("instructionsButton").style.display = "inline-block";
+});
+
+document.addEventListener("keydown", moveBear);
+
+//===========================================================================
+// And of course we need something to start it all off when the page loads!
+//=============================================================================
 drawForest();
-
-// //draw the row divs in the HTML so that we can later draw the images at them
-// function createHTMLRows() {
-//   for (let newRow = 0; newRow < maze.length; newRow++) {
-//     var newDiv = document.createElement("div");
-//     newDiv.id = "row" + newRow;
-//         newDiv.className = "mazeRow";
-//         var destination = document.getElementById("thisIsTheMaze");
-//         destination.appendChild(newDiv);
-//       }
-// }
-
-
-// function clearTheOldMaze() {
-//   for (let rowToClear = 0; rowToClear < maze.length; rowToClear++) {
-//     document.getElementById("row" + rowToClear).innerHTML = "";
-//   }
-// }
-
-
-// function redrawMaze() {
-//   clearTheOldMaze();
-//   //for every row
-//   for (let mazeRow = 0; mazeRow < maze.length; mazeRow++) {
-//     //for every column in that row
-//     for (
-//       let mazeColumnItem = 0;
-//       mazeColumnItem < maze[mazeRow].length;
-//       mazeColumnItem++
-//       ) {
-//         //draw the appropriate picture on the screen for each character.
-//         if (maze[mazeRow][mazeColumnItem] === "W") {
-//           drawMazeItem(treeImage, mazeRow);
-      
-//         } else if (maze[mazeRow][mazeColumnItem] === "S") {
-//           drawMazeItem(bearImage, mazeRow);
-//           startLocation = [mazeRow, mazeColumnItem];
-//         maze[mazeRow] = maze[mazeRow].replace("S", "B"); //changes the S to a B so that I should be able to move it.
-        
-//     } else if (maze[mazeRow][mazeColumnItem] === "F") {
-//       drawMazeItem(cabinImage, mazeRow);
-//       finishLocation = [mazeRow, mazeColumnItem];
-      
-//     } else if (maze[mazeRow][mazeColumnItem] === "B") {
-//       drawMazeItem(bearImage, mazeRow);
-      
-//     } else {
-//       drawMazeItem(blankImage, mazeRow);
-//     }
-//   }
-// }
-// }
-
-
-// function drawMazeItem(imageToDraw, mazeRow) {
-//   //make an image element  
-//   var img = document.createElement("IMG");
-//   //put the assigned image in that element
-//   img.src = imageToDraw;
-//   //create class name of "mazeItem" and the title of the image
-//   var imageName = imageToDraw.slice(4, -4);
-//   img.className = "mazeItem " + imageName;
-//   //append image to the correct row
-//   var destination = document.getElementById("row" + mazeRow);
-//   destination.appendChild(img);
-// }
-
-
-// function moveBear(e) {
-//   //hold where the bear WAS as an array that does not reference its source array 
-//   oldBearLocation = bearLocation.slice(0);
-  
-//   //look around the bear and determine what's next to him in all 4 directions
-//   var spaceUp = maze[bearLocation[0] - 1][bearLocation[1]];
-//   var spaceDown = maze[bearLocation[0] + 1][bearLocation[1]];
-//   var spaceRight = maze[bearLocation[0]][bearLocation[1] + 1];
-//   var spaceLeft = maze[bearLocation[0]][bearLocation[1] - 1];
-  
-//   checkForWin(spaceUp)
-//   checkForWin(spaceDown)
-//   checkForWin(spaceLeft)
-//   checkForWin(spaceRight)
-  
-//   //respond to directional keys
-//   if (e.key === "ArrowUp" || e.key === "W" || e.key === "w") {  //use arrow keys or WASD
-//     if (spaceUp == "W" || spaceUp == undefined) {  //make sure the target destination is open and legal
-//       return;
-//     }
-//     //move the B in the maze array one row up by slicing and concatenating the rows
-//     var bearColumnIndex = maze[oldBearLocation[0]].indexOf("B");
-    
-//     maze[oldBearLocation[0]] =              //puts a " " where the bear used to be
-//     maze[oldBearLocation[0]].slice(0, bearColumnIndex) +
-//     " " +
-//     maze[oldBearLocation[0]].slice(bearColumnIndex + 1);
-    
-//     maze[oldBearLocation[0] - 1] =          //puts a "B" where the bear used to be
-//     maze[oldBearLocation[0] - 1].slice(0, bearColumnIndex) +
-//     "B" +
-//     maze[oldBearLocation[0] - 1].slice(bearColumnIndex + 1);
-    
-//     bearLocation[0]--;                       //update bear location
-//     redrawMaze();
-//     return;
-    
-//   } else if (e.key === "ArrowDown" || e.key === "S" || e.key === "s") {
-//     if (spaceDown == "W" || spaceDown == undefined) {
-//       return;
-//     }
-//     var bearColumnIndex = maze[oldBearLocation[0]].indexOf("B");
-    
-//     maze[oldBearLocation[0]] =
-//     maze[oldBearLocation[0]].slice(0, bearColumnIndex) +
-//     " " +
-//     maze[oldBearLocation[0]].slice(bearColumnIndex + 1);
-    
-//     maze[oldBearLocation[0] + 1] =
-//     maze[oldBearLocation[0] + 1].slice(0, bearColumnIndex) +
-//     "B" +
-//     maze[oldBearLocation[0] + 1].slice(bearColumnIndex + 1);
-    
-//     bearLocation[0]++;
-//     redrawMaze();
-//     return;
-    
-//   } else if (e.key === "ArrowLeft" || e.key === "A" || e.key === "a") {
-//     if (spaceLeft == "W" || spaceLeft == undefined) {
-//       return;
-//     }
-//     maze[bearLocation[0]] = maze[bearLocation[0]].replace(" B", "B ");
-//     bearLocation[1]--;
-//     redrawMaze();
-//     return;
-    
-//   } else if (e.key === "ArrowRight" || e.key === "D" || e.key === "d") {
-//     if (spaceRight == "W" || spaceRight == undefined) {
-//       return;
-//     }
-//     maze[bearLocation[0]] = maze[bearLocation[0]].replace("B ", " B");
-//     bearLocation[1] = bearLocation[1] + 1;
-//     redrawMaze();
-//     return;
-//   }
-// }
-
-
-// function checkForWin(target) {
-//   if (target !== "F") {return}
-//   document.getElementById("youWon").style.display="flex"
-// }
-
-// function startOver(){
-//   window.location.reload()
-// }
-
-// createHTMLRows();
-// redrawMaze();
-// bearLocation = startLocation.slice(0);
-// oldBearLocation = bearLocation.slice(0);
-// document.addEventListener("keydown", moveBear);
-// document.getElementById("startOver").addEventListener("click", startOver)
