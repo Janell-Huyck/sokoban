e729f93a25747ca2657291f3a6ce6c9566eb7e0f
Janell Huyck <janell.huyck@outlook.com>
2019-09-29 23:39:17 -0400

    removed non-functional test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-//Sadly, I could not figure out how to get tests to work for this program beyond this one.
-//Since this was my first attempt at TDD, I'm going to try again with the next project!
-
-//cannot determine player location - location is underined
-// console.assert(bearLocation !== undefined, "We have lost the bear!  His location is 'undefined'!");
-
